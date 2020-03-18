efbacdf184d85eac18b3b8e442897a6ccd5a86fd
Christina Placide <christina.placide@hotmail.com>
2020-03-18 01:16:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -69,7 +69,7 @@ def monopoly_with_fourth_tier
         
         :mortgage_value => 400
       },
-      :shortline=>
+      :shortline_railroad=>
       {
         
         :mortgage_value => 800
@@ -78,7 +78,5 @@ def monopoly_with_fourth_tier
   }
 }
 
-
- monopoly
 
 end
